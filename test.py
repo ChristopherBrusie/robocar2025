7032b2c3fb1d88d4d55a0056adc2a04534a104e4
ChristopherBrusie <brusie@wustl.edu>
2025-06-25 23:12:26 -0500

    init

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-# Adjust the port to match your ESP32 (e.g., /dev/ttyUSB0 or /dev/ttyACM0)
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 time.sleep(2)  # Wait for ESP32 to reset
 
 while True:
+
+
+    # send speed input
     speed = input("Enter motor speed (0–100): ")
     if speed.isdigit():
         ser.write((speed + '\n').encode())
